dadceabb0a5b17e403b25f416129d7ad2a91ac92
Katleiah Ramos <katleiahramos@gmail.com>
2018-03-16 17:29:54 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -14,8 +14,9 @@ NEW = {}
 
   def add_student(student, grade)
     #binding.pry
-    NEW[grade] = [student] if !(NEW.include?(student))
+
     NEW[grade] << student if NEW.include?(grade)
+    NEW[grade] = [student] if !(NEW.include?(student))
 
   end
 
