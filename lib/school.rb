c80222f7fc8e2f0dc17a4d07841f967fbdd66267
Katleiah Ramos <katleiahramos@gmail.com>
2018-03-16 17:59:45 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -2,19 +2,21 @@ require 'pry'
 # code here!
 class School
 
-NEW = {}
 
   def initialize(name)
     @name = name
+    @roster = {}
   end
 
   def roster
-    NEW
+    @roster
   end
 
+
   def add_student(student, grade)
+
     NEW[grade] = [] unless NEW.include?(grade)
-    NEW[grade] << student  
+    NEW[grade] << student
     #NEW[grade] = [student] if !(NEW.include?(grade))
   end
 
